860d5a425f0b209a3b74e97a6ae8df7c7c6e7ba7
Ali <ali@mail.fr>
2020-03-13 15:38:51 +0100

    trucs et machins

--- ProjetArticlev0/src/main/java/controleur/SiteControlleur.java
+++ ProjetArticlev0/src/main/java/controleur/SiteControlleur.java
@@ -1,14 +1,20 @@
 package controleur;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import repository.ArticleRepository;
+
 @Controller
 public class SiteControlleur {
 
+	@Autowired
+	private ArticleRepository repository;
+	
 	@RequestMapping("/acc")
 	public String hello(Model model)
 	{
@@ -17,8 +23,10 @@ public class SiteControlleur {
 	@RequestMapping("/biblio")
 	public String hello2(Model model)
 	{
+		model.addAttribute("articles",repository.findAll());
 		return "site/biblio";
 	}
+	
 	@RequestMapping("/conn")
 	public String hello3(Model model)
 	{
